404cbe9bc8cb44c242a7629e94a6bd5d70135935
marcos <marcos.martins.filho@gmail.com>
2018-05-16 00:02:10 -0300

    new version

--- src/main/resources/static/init/initAluno.js
+++ src/main/resources/static/init/initAluno.js
@@ -142,54 +142,26 @@ $(document).ready(function() {
 		}
 	});
 	
-//	var substringMatcher = function(strs) {
-//		  return function findMatches(q, cb) {
-//		    var matches, substringRegex;
-//
-//		    // an array that will be populated with substring matches
-//		    matches = [];
-//
-//		    // regex used to determine if a string contains the substring `q`
-//		    substrRegex = new RegExp(q, 'i');
-//
-//		    // iterate through the pool of strings and for any string that
-//		    // contains the substring `q`, add it to the `matches` array
-//		    $.each(strs, function(i, str) {
-//		      if (substrRegex.test(str)) {
-//		        matches.push(str);
-//		      }
-//		    });
-//
-//		    cb(matches);
-//		  };
-//		};
-//
-//		var aCurso = new Array();
-//		$.ajax({
-//			    url: 'http://localhost:8080/curso/findAll',
-//			    async: true,
-//				type: 'GET',
-//				contentType: "application/json",
-//				success:function(data){
-//					
-//		            for( var i in data){
-//		                aCurso.push(data[i].nomeCurso);
-//		            }
-//				},
-//				error: function(data){			
-//					alunos = [];
-//				}
-//		    });
-//
-//		$('#cursos .typeahead').typeahead({
-//		  hint: true,
-//		  highlight: true,
-//		  minLength: 1
-//		},
-//		{
-//		  name: 'Cursos',
-//		  source: substringMatcher(aCurso)
-//		});
+	$.ajax({
+    url: 'http://localhost:8080/curso/findAll',
+    async: true,
+	type: 'GET',
+	contentType: "application/json",
+	success:function(data){
+		
+        console.log(data)
+			
+		$.each(data, function (i, item) {
+            $('#curso').append($('<option>', { 
+                value: item.siglaCurso,
+                text : item.nomeCurso 
+            }));
+        });
+	},
+	error: function(data){			
+		alunos = [];
+	}
+});
 	
 	$("#udpate").click(function() {
 		var obj;
